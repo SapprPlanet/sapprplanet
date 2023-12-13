374fe477845af4583d0b84e5cb55eb5bd7a9ef66
Alexey <83959065+batovalex@users.noreply.github.com>
2023-12-13 12:01:55 +0300

    Create __.init__.py

---
+++ magspheres/__.init__.py
@@ -0,0 +1,12 @@
+from .malkinsphere import make_views
+from .malkinsphere import make_movie
+from .malkinsphere import rotanimate
+del malkinsphere
+
+__author__ = "Anton Salnikov"
+
+__all__ = [
+    "make_views",
+    "make_movie",
+    "rotanimate",
+]
